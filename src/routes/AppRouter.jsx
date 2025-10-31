edd6a5f8fd0537f5a1a31762311c9f6fb930163f
YarceJose <tamayoyarce@gmail.com>
2025-10-31 10:43:03 -0500

    Se crearon las rutas entre carpetas

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,25 @@
+import { Login } from "../pages/auth/Login";
+import { Registro } from "../pages/auth/Registro";
+import { Dashboard } from "../pages/dashboard/Dashboard";
+import { Home } from "../pages/home/Home";
+
+
+export let AppRouter =[
+    {
+        path:"/login",
+        element:<Login />
+    },
+    {
+        path:"/registro",
+        element:<Registro />
+    },
+    {
+        path:"/dashboard",
+        element:<Dashboard />
+    },
+    {
+        path:"/",
+        element: <Home />
+    },
+
+]
